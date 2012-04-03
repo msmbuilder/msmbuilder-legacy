3d1961ba466a17a28ddce46eab19cbd66e9e06b0
Robert McGibbon <rmcgibbo@gmail.com>
2012-04-03 05:43:39 +0000

    added AssignMPI, added description text to Assign scripts

--- scripts/Assign.py
+++ scripts/Assign.py
@@ -11,7 +11,22 @@ from Emsmbuilder import metrics
 from Emsmbuilder import Project
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="""
+Assign data that were not originally used in the clustering (because of
+striding) to the microstates. This is applicable to all medoid-based clustering
+algorithms, which includes all those implemented by Cluster.py except the
+hierarchical methods. (For assigning to a hierarchical clustering, use 
+AssignHierarchical.py)
+
+Outputs:
+-Assignments.h5
+-Assignments.h5.distances
+
+Assignments.h5 contains the assignment of each frame of each trajectory to a 
+microstate in a rectangular array of ints. Assignments.h5.distances is an 
+array of real numbers of the same dimension containing the distance (according 
+to whichever metric you choose) from each frame to to the medoid of the 
+microstate it is assigned to.""", formatter_class=argparse.RawDescriptionHelpFormatter)
     add_argument(parser, '-p', dest='project', help='Path to ProjectInfo file',
         default='ProjectInfo.h5')
     add_argument(parser, '-g', dest='generators', help='''Output trajectory file containing
