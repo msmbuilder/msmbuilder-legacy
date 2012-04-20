8e000548d4bf7f63d9eb104f8f9ce740b3facc7b
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2012-04-20 20:06:12 +0000

    Major changes to tutorial

--- Tutorial/PlotMacrostateImpliedTimescales.py
+++ Tutorial/PlotMacrostateImpliedTimescales.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
+
+from numpy import *
+from matplotlib import *
+from matplotlib.pyplot import *
 
 Unit = "ps"
 NumStates = 4
 DataDir = "./Macro4/"
 LagTime = 1
 
-x,y=LagTime*loadtxt(DataDir+"/ImpliedTimescales.dat").transpose()
+x,y = LagTime*loadtxt(DataDir+"/ImpliedTimescales.dat").transpose()
 
 plot(x,y,'o')
 
@@ -12,8 +17,8 @@ title("Implied Timescales")
 xlabel("Lagtime")
 ylabel("Implied Timescale")
 
-K=loadtxt(DataDir+"/Rate.dat")
-lam,ev=eig(K)
+K = loadtxt(DataDir+"/Rate.dat")
+lam,ev = linalg.eig(K)
 lam = sort(abs(lam))
 for i in range(1,NumStates):
 	plot(x,LagTime/lam[i]*(x*0+1),'g-')
@@ -24,3 +29,7 @@ plot([],[],'bo',label="Fixed Lagtime")
 xlabel("Lagtime [%s]"%Unit)
 ylabel("Implied Timescale [%s]"%Unit)
 legend(loc=0,numpoints=1)
+
+axis([0,26,0,26])
+
+show()
