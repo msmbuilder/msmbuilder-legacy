2f770a86d5198471a97f56975c33e5a8bc2162a2
Lee-Ping Wang <leeping@stanford.edu>
2012-05-16 18:30:32 +0000

    Turned off debug printout from lprmsd.c SaveAlignedPDBs.py is even faster now (option for loading trajs into memory)

--- Extras/SaveAlignedPDBs.py
+++ Extras/SaveAlignedPDBs.py
@@ -23,10 +23,22 @@ from msmbuilder import arglib
 from msmbuilder import Trajectory
 from msmbuilder.metric_LPRMSD import LPRMSD, LPTraj, ReadPermFile
 from collections import defaultdict
+import copy
 import numpy as np
 import random
+import resource
 
-def run(project, assignments, conformations_per_state, states, output_dir, gens_file, atom_indices, permute_indices, alt_indices):
+def get_size(start_path = '.'):
+    total_size = 0
+    num_files = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+            num_files += 1
+    return num_files, total_size
+
+def run(project, assignments, conformations_per_state, states, output_dir, gens_file, atom_indices, permute_indices, alt_indices, total_memory):
     if states == "all":
         states = np.arange(assignments.max()+1)
     # This is a dictionary: {generator : ((traj1, frame1), (traj1, frame3), (traj2, frame1), ... )}
@@ -46,6 +58,18 @@ def run(project, assignments, conformations_per_state, states, output_dir, gens_
     digits = len(str(max(states)))
     formstr_pdb = '\"Generator-%%0%ii.pdb\"' % digits
     formstr_xtc = '\"Cluster-%%0%ii.xtc\"' % digits
+    print "Loading up the trajectories."
+    traj_nfiles, traj_bytes = get_size(project['TrajFilePath'])
+    LoadAll = 0
+    MaxMem = 0.0
+    # LPW This is my hack that decides whether to load trajectories into memory, or to read them from disk.
+    if (traj_bytes * 5) < total_memory * 1073741824: # It looks like the Python script uses roughly 5x the HDF file size in terms of memory.
+        print "Loading all trajectories into memory."
+        LoadAll = 1
+        AllTraj = [project.LoadTraj(i) for i in np.arange(project["NumTrajs"])]
+        print "After loading trajectories, memory usage is % .3f GB" % (float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) / 1048576)
+
+    cluster_traj = project.GetEmptyTrajectory()
     # Loop through the generators.
     for s in states:
         if len(inverse_assignments[s]) == 0:
@@ -64,12 +88,17 @@ def run(project, assignments, conformations_per_state, states, output_dir, gens_
             FrameDict.setdefault(traj,[]).append(frame)
         # Create a single trajectory corresponding to the frames that
         # belong to the current generator.
-        cluster_traj = project.GetEmptyTrajectory()
+        if "XYZList" in cluster_traj:
+            cluster_traj.pop("XYZList")
+        print "Generator %i" % s,
         TrajNums = set([i[0] for i in confs])
         for i in TrajNums:
-            T = project.LoadTraj(i)[np.array(FrameDict[i])]
+            if LoadAll:
+                T = AllTraj[i][np.array(FrameDict[i])]
+            else:
+                T = project.LoadTraj(i)[np.array(FrameDict[i])]
             cluster_traj += T
-        print "Loaded %i conformations for generator %i, now aligning." % (len(cluster_traj), s)
+        print " loaded %i conformations, aligning" % len(cluster_traj),
         # Prepare the trajectory, align to the generator, and reassign the coordinates.
         p_cluster_traj = rmsd_metric.prepare_trajectory(cluster_traj)
         rmsd, xout = rmsd_metric.one_to_all_aligned(p_gens_traj, p_cluster_traj, s)
@@ -78,9 +107,15 @@ def run(project, assignments, conformations_per_state, states, output_dir, gens_
         outpdb = eval(formstr_pdb) % s
         outxtc = eval(formstr_xtc) % s
         this_gen_traj = p_gens_traj[s]
-        print "Done aligning; saving cluster to %s / %s" % (os.path.join(output_dir,outpdb), os.path.join(output_dir,outxtc))
+        print ", saving PDB to %s" % os.path.join(output_dir,outpdb),
         this_gen_traj.SaveToPDB(os.path.join(output_dir,outpdb))
+        print ", saving XTC to %s" % os.path.join(output_dir,outxtc),
         p_cluster_traj.SaveToXTC(os.path.join(output_dir,outxtc))
+        print ", saved"
+        NowMem = float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) / 1048576
+        if NowMem > MaxMem:
+            MaxMem = NowMem
+    print "This script used at least % .3f GB of memory" % MaxMem
                 
 if __name__ == '__main__':
     parser = arglib.ArgumentParser(description="""
@@ -108,6 +143,7 @@ to use GetRandomConfs.py""")
     a list of newline separated indices with a "--" separating the sets of indices if there are
     more than one set of indistinguishable atoms''')
     parser.add_argument('lprmsd_alt_indices', default='None', description='Alternate atom indices')
+    parser.add_argument('total_memory_gb', default=4, type=int, description='Available memory in GB; this determines whether to load all trajectories into memory or to read them one-by-one from disk.')
 
     parser.add_argument('generators', description='''Trajectory file containing
     the structures of each of the cluster centers.  Produced using Cluster.py.''', default='Data/Gens.lh5')
@@ -137,5 +173,5 @@ to use GetRandomConfs.py""")
         alt_indices = np.loadtxt(args.lprmsd_alt_indices, np.int)
 
     run(args.project, args.assignments['Data'], args.conformations_per_state,
-         args.states, args.output_dir, args.generators, atom_indices, permute_indices, alt_indices)
+         args.states, args.output_dir, args.generators, atom_indices, permute_indices, alt_indices, args.total_memory_gb)
 
