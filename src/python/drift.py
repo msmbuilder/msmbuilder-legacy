5d4bcbd4ab69170d0d53a6b2733c15fe2e8625fa
Robert McGibbon <rmcgibbo@gmail.com>
2012-08-30 16:57:15 -0700

    docstring formatting in drift.py

--- src/python/drift.py
+++ src/python/drift.py
@@ -3,36 +3,44 @@ Compute the drift in trajectories under different distance metrics
 using new distance_metrics.py code
 """
 import numpy as np
-#from msmbuilder.metrics import AbstractDistanceMetric
 
 
 def _drift_single_trajectory(metric, trajectory, tau):
     """
-    Compute the drift in your desired metric between all pairs of conformations
-    in the supplied trajectory which are seperated by tau frames.
+    Compute the drift in your desired metric between all pairs of
+    conformations in the supplied trajectory which are seperated
+    by tau frames.
     
-    tau can be either a positive integer or an array of positive
-    integers.
-    
-    If tau is an integer, the return value is a 1D array of length equal
-    to the length of the trajectory minus tau containing the pairwise
-    distance of all conformations in trajectory seperated by tau frames.
-    
-    If tau is an array of integers of length n, the return value is a 2D
-    array with n rows and a number of columns equal to the length of the
-    trajectory minus min(tau). The ith row of the returned array contains
-    the pairwise distance of all of the conformations in the supplied trajectory
-    separated by tau[i] frames. The final i entries in row i will be padded
-    with -1s to ensure that the output 2D arrau is rectangular.
-    
-    prepare_kwargs=
-    
-    measure_kwargs=
+    Parameters
+    ----------
+    metric : msmbuilder.metrics.AbstractDistanceMetric
+        The distance metric to compute distances with
+    trajectory : msmbuilder.Trajectory
+        The trajectory to compute the distances
+    tau : {int, np.ndarray}
+        tau can be either a positive integer or an array of positive
+        integers. If tau is an integer, the return value is a 1D array
+        of length equal to the length of the trajectory minus tau
+        containing the pairwise distance of all conformations in
+        trajectory seperated by tau frames. If tau is an array of
+        integers of length n, the return value is a 2D array with n
+        rows and a number of columns equal to the length of the
+        trajectory minus min(tau). The ith row of the returned array
+        contains the pairwise distance of all of the conformations in
+        the supplied trajectory separated by tau[i] frames. The final
+        i entries in row i will be padded with -1s to ensure that the
+        output 2D arrau is rectangular.
+        
+    Returns
+    -------
+    distances : np.ndarray
+        1D or 2D array of the drifts, depending on whether tau is a single
+        number or an array
     """
     # make sure tau is a 1D numpy array of positive ints, or make it into one
     tau = __typecheck_tau(tau)
- #   if not isinstance(metric, AbstractDistanceMetric):
- #       raise TypeError('metric must be an instance of AbstractDistanceMetric. you supplied a %s' % metric)
+    #if not isinstance(metric, AbstractDistanceMetric):
+    #   raise TypeError('metric must be an instance of AbstractDistanceMetric. you supplied a %s' % metric)
     
     traj_length = trajectory['XYZList'].shape[0]
     ptraj = metric.prepare_trajectory(trajectory)
@@ -61,7 +69,7 @@ def drift(metric, trajectories, taus):
         d = _drift_single_trajectory(metric, trajectory, taus)
         for i in range(len(taus)):
             #length_i = d.shape[1] - (taus[i] - np.min(taus))
-            selected_d = np.ma.masked_less(d[i,:], 0)
+            selected_d = np.ma.masked_less(d[i, :], 0)
             selected_d = np.ma.compressed(selected_d)
             output[i] = np.hstack((output[i], selected_d))
             
@@ -74,17 +82,20 @@ def square_drift(metric, trajectories, tau):
     return output
 
 
-def get_epsilon_neighborhoods(metric,ptraj,tau):
+def get_epsilon_neighborhoods(metric, ptraj, tau):
 
     output = []
     N = len(ptraj)
     for i in xrange(N):
         if i < tau:
-            output.append( metric.one_to_many(ptraj,ptraj,i,[i+tau])[0] )
+            output.append(metric.one_to_many(ptraj, ptraj,
+                i, [i + tau])[0])
         elif i >= N-tau:
-            output.append( metric.one_to_many(ptraj,ptraj,i,[i-tau])[0] )
+            output.append(metric.one_to_many(ptraj, ptraj, i,
+                [i - tau])[0])
         else:
-            output.append( metric.one_to_many(ptraj,ptraj,i,[i-tau,i+tau]).max() )
+            output.append(metric.one_to_many(ptraj, ptraj, i,
+                [i - tau, i + tau]).max())
     
     return output
 
@@ -111,7 +122,8 @@ def hitting_time(metric, trajectory, epsilon):
         while not found:
             forward_window = np.arange(start, start + window_length * mult)
             forward_window = forward_window[np.where(forward_window < traj_length)]
-            if len(forward_window) == 0: break
+            if len(forward_window) == 0:
+                break
             #print forward_window
             d = metric.one_to_many(ptraj, ptraj, i, forward_window)
             where = np.where(d > epsilon)[0]
@@ -136,7 +148,9 @@ def __typecheck_tau(tau):
     or make it into one if possible unambiguously"""
     
     if isinstance(tau, int):
-        if tau < 0: raise TypeError('Tau cannot be negative')
+        if tau < 0:
+            raise TypeError('Tau cannot be negative')
+        
         tau = np.array([tau])
     else:
         tau = np.array(tau)
