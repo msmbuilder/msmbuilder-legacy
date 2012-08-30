3c5bfebd838236ba44eecd3c7b33a3618806f8a6
Robert McGibbon <rmcgibbo@gmail.com>
2012-08-30 16:20:02 -0700

    pep8 method names in rg.py

--- src/python/geometry/rg.py
+++ src/python/geometry/rg.py
@@ -1,25 +1,8 @@
 import numpy as np
 from msmbuilder import _rg_wrap
+from msmbuilder.utils import deprecated
 
-def Reference_Rg(xyzlist):
-    '''Return the radius of gyration of every frame in a xyzlist'''
-    
-    traj_length = len(xyzlist)
-    Rg = np.zeros(traj_length)
-    for i in xrange(traj_length):
-        XYZ = xyzlist[i,:,:]
-        mu = XYZ.mean(0)
-        XYZ2 = XYZ-np.tile(mu,(len(XYZ),1))
-        Rg[i] = (XYZ2**2.).mean()**0.5
-        
-        #print "xyz2", (XYZ2**2.0).mean()
-        #print "frame", i
-        #print "mean", mu
-    
-    return Rg
-    
-    
-def Rg(xyzlist):
+def calculate_rg(xyzlist):
     '''
     Compute the Rg for every frame in an xyzlist
     
@@ -37,16 +20,8 @@ def Rg(xyzlist):
     results = np.zeros(traj_length, dtype=np.double)
     _rg_wrap.rg_wrap(xyzlist, results)
     return results
-    
-    
-if __name__ == '__main__':
-    from msmbuilder.Project import Project
-    project = Project.LoadFromHDF('/home/rmcgibbo/Tutorial/ProjectInfo.h5')
-    traj = project.LoadTraj(0)
-    
-    xyzlist = traj['XYZList']#[0:10,:,:]
-    
-    a = Reference_Rg(xyzlist)
-    b = Rg(xyzlist)
-    
-    print a - b
+
+
+@deprecated(calculate_rg, '2.7')
+def Rg():
+    pass
