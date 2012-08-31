1692476be4b3d29a5c39f3a5c71cb8145b3f076e
tjlane <thomas.joseph.lane@gmail.com>
2012-08-30 19:10:26 -0700

    bye to my Extras, working on FahProject

--- Extras/InstallExtras.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=$PYTHONPATH:$MSMBUILDERHOME/Extras/
-
-#Run this script to append the Extras directory to your pythonpath.
-#Allows you to use "experimental" msmbuilder features.
