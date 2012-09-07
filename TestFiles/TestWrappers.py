5fa57d1964a117daf9b46f8f6f1e65da73ed6f1c
Christian Schwantes <schwancr@stanford.edu>
2012-09-06 17:09:12 -0700

    Bugfix in TestWrappers to asser the arrays are 'almost' equal as opposed to actually equal

--- TestFiles/TestWrappers.py
+++ TestFiles/TestWrappers.py
@@ -122,8 +122,8 @@ class TestWrappers(unittest.TestCase):
 
         r_tICA = Serializer.LoadFromHDF( tICADataFn )
 
-        numpy.testing.assert_array_equal( tICA['vecs'], r_tICA['vecs'] )
-        numpy.testing.assert_array_equal( tICA['vals'], r_tICA['vals'] )
+        numpy.testing.assert_array_almost_equal( tICA['vecs'], r_tICA['vecs'] )
+        numpy.testing.assert_array_almost_equal( tICA['vals'], r_tICA['vals'] )
 
     def test_c_Cluster(self):
         # We need to be sure to skip the stochastic k-mediods
