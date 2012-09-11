96ad131e3c891a3e4e7e45a66db451129ede1dda
Robert McGibbon <rmcgibbo@gmail.com>
2012-09-10 22:31:22 -0700

    Reimplementation of CalculateClusterRadii.py -- making it more efficient

--- scripts/CalculateProjectSASA.py
+++ scripts/CalculateProjectSASA.py
@@ -37,7 +37,7 @@ def run(project, atom_indices=None, traj_fn = 'all'):
             traj_fn = project.GetTrajFilename( traj_ind )
             chunk_ind = 0
             for traj_chunk in Trajectory.EnumChunksFromLHDF( traj_fn, AtomIndices=atom_indices ):
-                print chunk_ind
+                #print chunk_ind
                 traj_asa.extend( asa.calculate_asa( traj_chunk, n_sphere_points = 24 ) )
                 chunk_ind += 1
             SASA[ traj_ind, : project['TrajLengths'][traj_ind] ] = traj_asa
