3de6440c719ff95c4d11d1f24f15a06382fffb13
Christian Schwantes <schwancr@stanford.edu>
2012-10-29 15:51:40 -0700

    Bugfix in __init__. logging.StreamHandler's kwarg 'stream' is stream in 2.7 but strm in 2.6

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -36,7 +36,7 @@ __all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-sh = logging.StreamHandler(stream=sys.stdout)
+sh = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter(fmt='%(asctime)s - %(message)s', datefmt="%H:%M:%S")
 sh.setFormatter(formatter)
 logger.addHandler(sh)
