Fix to the rg calculation, issue #88

tests/test_rg.py
@@ -8,12 +8,13 @@ def reference_rg(xyzlist):
     '''Return the radius of gyration of every frame in a xyzlist'''
     
     traj_length = len(xyzlist)
+    n_atoms = xyzlist.shape[1]
     Rg = np.zeros(traj_length)
     for i in xrange(traj_length):
         XYZ = xyzlist[i, :, :]
         mu = XYZ.mean(0)
         XYZ2 = XYZ - np.tile(mu, (len(XYZ), 1))
-        Rg[i] = (XYZ2**2.0).mean()**0.5
+        Rg[i] = ((XYZ2**2.0).sum()/n_atoms)**0.5
     
     return Rg
 