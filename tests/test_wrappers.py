78315fbc150bb85acc19e0c199fabea9a4147509
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-01-13 17:38:34 -0800

    pep8 fixes for test_wrappers.py

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -30,7 +30,7 @@ import tarfile
 
 from msmbuilder import MSMLib
 from msmbuilder import Trajectory
-from msmbuilder.testing import get,eq,load
+from msmbuilder.testing import get, eq, load
 
 from msmbuilder.scripts import ConvertDataToHDF
 from msmbuilder.scripts import CreateAtomIndices
@@ -54,12 +54,14 @@ from msmbuilder.scripts import CalculateProjectDistance
 def pjoin(*args):
     return os.path.join(*args)
 
-# subclassing this will get you a test where you have a temporary 
+
+# subclassing this will get you a test where you have a temporary
 # directory (self.td) that will get cleaned up when you tes finishes
 # (whether it passes or fails)
 class WTempdir(object):
     def setup(self):
         self.td = tempfile.mkdtemp()
+
     def teardown(self):
         shutil.rmtree(self.td)
 
@@ -71,7 +73,7 @@ class test_ConvertDataToHDF(WTempdir):
         fh = tarfile.open(xtc_fn, mode='r:gz')
         fh.extractall(self.td)
         fh.close()
-    
+
         outfn = pjoin(self.td, 'ProjectInfo.yaml')
         # mode to that directory
         os.chdir(self.td)
@@ -88,7 +90,7 @@ class test_ConvertDataToHDF(WTempdir):
 
 def test_CreateAtomIndices():
     indices = CreateAtomIndices.run(get('native.pdb', just_filename=True),
-                                   'minimal')    
+                                   'minimal')
     eq(indices, get('AtomIndices.dat'))
 
 
@@ -100,7 +102,7 @@ class test_Cluster_kcenters(WTempdir):
             '-a', pjoin(self.td, 'Assignments.h5'),
             '-d', pjoin(self.td, 'Assignments.h5.distances'),
             '-g', pjoin(self.td, 'Gens.lh5'),
-            'rmsd', '-a', get('AtomIndices.dat', just_filename=True), 
+            'rmsd', '-a', get('AtomIndices.dat', just_filename=True),
             'kcenters', '-k', '100'], print_banner=False)
         Cluster.main(args, metric)
 
@@ -113,19 +115,17 @@ class test_Cluster_kcenters(WTempdir):
 
 
 class test_Cluster_hierarchical(WTempdir):
-   # this one tests hierarchical
-   def test(self):
-       args, metric = Cluster.parser.parse_args([
-           '-p', get('ProjectInfo.yaml', just_filename=True),
-           '-s', '10',
-           '-g', pjoin(self.td, 'Gens.lh5'),
-           'rmsd', '-a', get('AtomIndices.dat', just_filename=True), 
-           'hierarchical', '-o', pjoin(self.td, 'ZMatrix.h5')],
+    def test(self):
+        args, metric = Cluster.parser.parse_args([
+            '-p', get('ProjectInfo.yaml', just_filename=True),
+            '-s', '10',
+            '-g', pjoin(self.td, 'Gens.lh5'),
+            'rmsd', '-a', get('AtomIndices.dat', just_filename=True),
+            'hierarchical', '-o', pjoin(self.td, 'ZMatrix.h5')],
             print_banner=False)
-       Cluster.main(args, metric)
+        Cluster.main(args, metric)
 
-       eq(load(pjoin(self.td, 'ZMatrix.h5')),
-            get('ZMatrix.h5'))
+        eq(load(pjoin(self.td, 'ZMatrix.h5')), get('ZMatrix.h5'))
 
 
 class test_Assign(WTempdir):
@@ -134,10 +134,10 @@ class test_Assign(WTempdir):
             '-p', get('ProjectInfo.yaml', just_filename=True),
             '-g', get('Gens.lh5', just_filename=True),
             '-o', self.td,
-            'rmsd', '-a', get('AtomIndices.dat', just_filename=True)], 
+            'rmsd', '-a', get('AtomIndices.dat', just_filename=True)],
             print_banner=False)
         Assign.main(args, metric)
-        
+
         eq(load(pjoin(self.td, 'Assignments.h5')),
            get('Assignments.h5'))
         eq(load(pjoin(self.td, 'Assignments.h5.distances')),
@@ -147,7 +147,7 @@ class test_Assign(WTempdir):
 def test_AssignHierarchical():
     asgn = AssignHierarchical.main(k=100, d=None,
         zmatrix_fn=get('ZMatrix.h5', just_filename=True))
-    
+
     eq(asgn, get('WardAssignments.h5')['Data'])
 
 
@@ -155,34 +155,36 @@ class test_BuildMSM(WTempdir):
     def test(self):
         BuildMSM.run(LagTime=1, assignments=get('Assignments.h5')['arr_0'], Symmetrize='MLE',
             OutDir=self.td)
-        
+
         eq(load(pjoin(self.td, 'tProb.mtx')), get('tProb.mtx'))
         eq(load(pjoin(self.td, 'tCounts.mtx')), get('tCounts.mtx'))
         eq(load(pjoin(self.td, 'Mapping.dat')), get('Mapping.dat'))
         eq(load(pjoin(self.td, 'Assignments.Fixed.h5')), get('Assignments.Fixed.h5'))
         eq(load(pjoin(self.td, 'Populations.dat')), get('Populations.dat'))
-        
+
+
 def test_CalculateImpliedTimescales():
     impTimes = CalculateImpliedTimescales.run(MinLagtime=3, MaxLagtime=5,
         Interval=1, NumEigen=10, AssignmentsFn=get('Assignments.h5', just_filename=True),
         trimming=True, symmetrize='Transpose', nProc=1)
-    
+
     eq(impTimes, get('ImpliedTimescales.dat'))
 
 
 def test_CalculateMFPTs(mfpt_state=70):
-    mfpt = CalculateMFPTs.run(get('tProb.mtx'),mfpt_state)
-    mfpt0 = get(pjoin("transition_path_theory_reference","mfpt.h5"))['Data']    
+    mfpt = CalculateMFPTs.run(get('tProb.mtx'), mfpt_state)
+    mfpt0 = get(pjoin("transition_path_theory_reference", "mfpt.h5"))['Data']
     eq(mfpt, mfpt0)
+
 
 def test_CalculateTPT():
     T = get("tProb.mtx")
-    sources = [0] # chosen arb. for ref. by TJL
+    sources = [0]  # chosen arb. for ref. by TJL
     sinks = [70]  # chosen arb. for ref. by TJL
     script_out = CalculateTPT.run(T, sources, sinks)
-    committors_ref = get( pjoin("transition_path_theory_reference", 
+    committors_ref = get(pjoin("transition_path_theory_reference",
                           "committors.h5"))['Data']
-    net_flux_ref = get( pjoin("transition_path_theory_reference", 
+    net_flux_ref = get(pjoin("transition_path_theory_reference",
                         "net_flux.h5"))['Data']
     eq(script_out[0], committors_ref)
     eq(script_out[1].toarray(), net_flux_ref)
@@ -201,31 +203,31 @@ def test_FindPaths():
     sinks = [70]
     paths, bottlenecks, fluxes = FindPaths.run(tprob, sources, sinks, 10)
     # paths are hard to test due to type issues, adding later --TJL
-    bottlenecks_ref = get( pjoin("transition_path_theory_reference", 
+    bottlenecks_ref = get(pjoin("transition_path_theory_reference",
                            "dijkstra_bottlenecks.h5"))['Data']
-    fluxes_ref = get( pjoin("transition_path_theory_reference", 
+    fluxes_ref = get(pjoin("transition_path_theory_reference",
                       "dijkstra_fluxes.h5"))['Data']
     eq(bottlenecks, bottlenecks_ref)
     eq(fluxes, fluxes_ref)
 
 
 class test_PCCA(WTempdir):
-    
+
     def test(self):
-        
+
         num_macro = 5
-        
+
         TC = get("PCCA_ref/tProb.mtx")
-        A  = get("PCCA_ref/Assignments.Fixed.h5")['arr_0']
+        A = get("PCCA_ref/Assignments.Fixed.h5")['arr_0']
         print A
-        
+
         macro_map, macro_assign = PCCA.run_pcca(num_macro, A, TC)
         r_macro_map = get("PCCA_ref/MacroMapping.dat")
 
         macro_map = macro_map.astype(np.int)
         r_macro_map = r_macro_map.astype(np.int)
-        
-        # The order of macrostates might be different between the reference and 
+
+        # The order of macrostates might be different between the reference and
         # new lumping. We therefore find a permutation to match them.
         permutation_mapping = np.zeros(macro_assign.max() + 1, 'int')
         for i in range(num_macro):
@@ -234,9 +236,9 @@ class test_PCCA(WTempdir):
 
         macro_map_permuted = permutation_mapping[macro_map]
         MSMLib.apply_mapping_to_assignments(macro_assign, permutation_mapping)
-        
+
         r_macro_assign = get("PCCA_ref/MacroAssignments.h5")['arr_0']
-        
+
         eq(macro_map_permuted, r_macro_map)
         eq(macro_assign, r_macro_assign)
 
@@ -270,6 +272,3 @@ class test_SaveStructures(WTempdir):
         for name in names:
             t = Trajectory.load_trajectory_file(pjoin(self.td, name))
             eq(t, get('save_structures/' + name))
-
-
-
