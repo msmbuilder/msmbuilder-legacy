ccf73879d956f2825186bc716998602bd0cbb8de
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-01-29 14:57:19 -0800

    pep8 fixes in BuildMSM.py

--- scripts/BuildMSM.py
+++ scripts/BuildMSM.py
@@ -26,6 +26,7 @@ from msmbuilder import MSMLib
 import logging
 logger = logging.getLogger('msmbuilder.scripts.BuildMSM')
 
+
 def run(lagtime, assignments, symmetrize='MLE', input_mapping="None", out_dir="./Data/"):
 
     # set the filenames for output
@@ -34,7 +35,7 @@ def run(lagtime, assignments, symmetrize='MLE', input_mapping="None", out_dir=".
     FnMap = os.path.join(out_dir, "Mapping.dat")
     FnAss = os.path.join(out_dir, "Assignments.Fixed.h5")
     FnPops = os.path.join(out_dir, "Populations.dat")
-    
+
     # make sure none are taken
     outputlist = [FnTProb, FnTCounts, FnMap, FnAss, FnPops]
     arglib.die_if_path_exists(outputlist)
@@ -43,26 +44,26 @@ def run(lagtime, assignments, symmetrize='MLE', input_mapping="None", out_dir=".
     if input_mapping != "None":
         MSMLib.apply_mapping_to_assignments(assignments, input_mapping)
 
-    n_assigns_before_trim = len( np.where( assignments.flatten() != -1 )[0] )
-    
+    n_assigns_before_trim = len(np.where(assignments.flatten() != -1)[0])
+
     counts = MSMLib.get_count_matrix_from_assignments(assignments, lag_time=lagtime, sliding_window=True)
-    
+
     rev_counts, t_matrix, populations, mapping = MSMLib.build_msm(counts, symmetrize=symmetrize, ergodic_trimming=True)
 
     MSMLib.apply_mapping_to_assignments(assignments, mapping)
-    n_assigns_after_trim = len( np.where( assignments.flatten() != -1 )[0] )
+    n_assigns_after_trim = len(np.where(assignments.flatten() != -1)[0])
 
     # if had input mapping, then update it
     if input_mapping != "None":
         mapping = mapping[input_mapping]
-    
+
     # Print a statement showing how much data was discarded in trimming
     percent = (1.0 - float(n_assigns_after_trim) / float(n_assigns_before_trim)) * 100.0
     logger.warning("Ergodic trimming discarded: %f percent of your data", percent)
- 
+
     # Save all output
     np.savetxt(FnPops, populations)
-    np.savetxt(FnMap, mapping,"%d")
+    np.savetxt(FnMap, mapping, "%d")
     scipy.io.mmwrite(str(FnTProb), t_matrix)
     scipy.io.mmwrite(str(FnTCounts), rev_counts)
     msmbuilder.io.saveh(FnAss, assignments)
@@ -74,33 +75,33 @@ def run(lagtime, assignments, symmetrize='MLE', input_mapping="None", out_dir=".
 
 if __name__ == "__main__":
     parser = arglib.ArgumentParser(description=
-"""Estimates the counts and transition matrices from an
-Assignments.h5 file. Reversible models can be calculated either from naive
-symmetrization or estimation of the most likely reversible matrices (MLE,
-recommended). Also calculates the equilibrium populations for the model
-produced. Outputs will be saved in the directory of your input Assignments.h5
-file.
-\nOutput: tCounts.mtx, tProb.mtx, Populations.dat,  Mapping.dat,
-Assignments.Fixed.h5, tCounts.UnSym.mtx""")
+                                   """Estimates the counts and transition matrices from an
+                                   Assignments.h5 file. Reversible models can be calculated either from naive
+                                   symmetrization or estimation of the most likely reversible matrices (MLE,
+                                   recommended). Also calculates the equilibrium populations for the model
+                                   produced. Outputs will be saved in the directory of your input Assignments.h5
+                                   file.
+                                   \nOutput: tCounts.mtx, tProb.mtx, Populations.dat,  Mapping.dat,
+                                   Assignments.Fixed.h5, tCounts.UnSym.mtx""")
     parser.add_argument('assignments')
     parser.add_argument('symmetrize', help="""Method by which to estimate a
-        symmetric counts matrix. Symmetrization ensures reversibility, but may skew
-        dynamics. We recommend maximum likelihood estimation (MLE) when tractable,
-        else try Transpose. It is strongly recommended you read the documentation
-        surrounding this choice.""", default='MLE',
-        choices=['MLE', 'Transpose', 'None'])
+                        symmetric counts matrix. Symmetrization ensures reversibility, but may skew
+                        dynamics. We recommend maximum likelihood estimation (MLE) when tractable,
+                        else try Transpose. It is strongly recommended you read the documentation
+                        surrounding this choice.""", default='MLE',
+                        choices=['MLE', 'Transpose', 'None'])
     parser.add_argument('lagtime', help='''Lag time to use in model (in
         number of snapshots. EG, if you have snapshots every 200ps, and set the
         lagtime=50, you'll get a model with a lagtime of 10ns)''', type=int)
     parser.add_argument('mapping', help='''Mapping, EG from microstates to macrostates. If given, this mapping will be applied to the specified assignments before creating an MSM.''', default="None")
     parser.add_argument('output_dir')
     args = parser.parse_args()
-    
+
     try:
         assignments = msmbuilder.io.loadh(args.assignments, 'arr_0')
     except KeyError:
         assignments = msmbuilder.io.loadh(args.assignments, 'Data')
-   
+
     if args.mapping != "None":
         args.mapping = np.array(np.loadtxt(args.mapping), dtype=int)
 
