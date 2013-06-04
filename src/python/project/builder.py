bugfix in updating project that was giving new trajectories the wrong index to save to. Also some other minor changes

src/python/project/builder.py
@@ -68,7 +68,6 @@ class ProjectBuilder(object):
                 self.output_traj_basename = 'trj'
 
         self.stride = kwargs.pop('stride', 1)
-        self.project = kwargs.pop('project', None)
         self.project_updated = False
         # Keep track of when we've updated everything
 
@@ -183,20 +182,30 @@ class ProjectBuilder(object):
             old_traj_locs = []
         old_traj_locs = np.array(old_traj_locs)
 
-        for i, file_list in enumerate(self._input_trajs()):
-            
+        num_trajs_added = 0
+        if self.project is None:
+            num_orig_trajs = 0
+        else:
+            num_orig_trajs = len(self.project._traj_paths)
+    
+        for file_list in self._input_trajs():
+
+            i = num_orig_trajs + num_trajs_added 
+            # index for a new trajectory file is total we currently have (since it's zero-indexed)
+
             error = None
 
-            traj_loc = '/'.join([d for d in os.path.relpath(file_list[0]).split('/') if not d in ['.', '..'] ][:-1])
+            traj_loc = '/'.join([d for d in os.path.relpath(file_list[0]).split('/') if not d in ['.', '..']][:-1])
             num_files = len(file_list)
             if not traj_loc in old_traj_locs:
                 try:
                     traj = self._load_traj(file_list)
                     traj["XYZList"] = traj["XYZList"][::self.stride]
                 except TypeError as e:
-                    traj_errors.append(e)
+                    #traj_errors.append(e)
                     logger.warning('Could not convert %d files from %s (%s)', num_files, traj_loc, e)
                 else:
+                    num_trajs_added += 1
                     lh5_fn = os.path.join(self.output_traj_dir, 
                                       (self.output_traj_basename + str(i) + self.output_traj_ext))
                     traj.save(lh5_fn)
@@ -213,7 +222,7 @@ class ProjectBuilder(object):
                         logger.error("%s (%d files), length %d, converted to %s with error '%s'", 
                                      traj_loc, num_files, traj_lengths[-1], lh5_fn, e)
 
-                traj_errors.append(error)
+                    traj_errors.append(error)
 
             else:  # Then this trajectory has been seen before, and so we need to 
                    # either extend it or skip it 
@@ -225,7 +234,7 @@ class ProjectBuilder(object):
                 old_ind = np.where(old_traj_locs == traj_loc)[0][0]
                 old_locs = self.project._traj_converted_from[old_ind]
                 old_num_files = len(old_locs)
-        
+
                 if old_num_files == len(file_list):
                     # Just assume if it is the same number of files then they are the
                     # same. We should change this eventually
@@ -277,12 +286,10 @@ class ProjectBuilder(object):
                     logger.warn('Fewer frames found than currently have. Skipping. (%s)' % traj_loc)
                     continue
 
-        #if len(traj_paths) == 0:
-        #    os.rmdir(self.output_traj_dir)
-        #    raise RuntimeError('No conversion jobs found!')
+        if len(traj_paths) == 0 and self.project is None:
+            os.rmdir(self.output_traj_dir)
+            raise RuntimeError('No conversion jobs found!')
 
-        #print traj_lengths, traj_paths, traj_errors, traj_converted_from
-        #print self.project._traj_lengths, self.project._traj_paths, self.project._traj_errors, self.project._traj_converted_from
         if not self.project is None:
             traj_lengths = list(self.project._traj_lengths) + traj_lengths
             traj_paths = list(self.project._traj_paths) + traj_paths
@@ -290,7 +297,6 @@ class ProjectBuilder(object):
             traj_converted_from = list(self.project._traj_converted_from) + traj_converted_from
             traj_converted_from = [[str(i) for i in l] for l in traj_converted_from]
 
-        #print traj_lengths, traj_paths, traj_errors, traj_converted_from
         self.project = Project({'conf_filename': self.conf_filename,
                                 'traj_lengths': traj_lengths,
                                 'traj_paths': traj_paths,