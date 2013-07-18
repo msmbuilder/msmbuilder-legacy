dd0b9d55d0cd2953ecb76c498e6ce648ca63c426
Matthew Harrigan <harrigan@stanford.edu>
2013-07-18 15:09:10 -0700

    Fixed script to get and plot dihedral angles

--- Tutorial/GetDihedrals.py
+++ Tutorial/GetDihedrals.py
@@ -1,4 +1,5 @@
-def GetPsiAtomIndicesAndPosition(C1):
+
+def GetAtomIndicesAndPosition(C1):
     a=C1["AtomNames"]
     aC=np.where(a=="C")[0]
     aN=np.where(a=="N")[0]
@@ -6,6 +7,7 @@ def GetPsiAtomIndicesAndPosition(C1):
     X=C1["XYZ"]
     NumResi=C1.GetNumberOfResidues()
     return(NumResi,a,aC,aN,aCA,X)
+
 
 def GetAllPsi(C1,NumResi=None,a=None,aC=None,aN=None,aCA=None,X=None):
     if a==None:
@@ -20,14 +22,7 @@ def GetAllPsi(C1,NumResi=None,a=None,aC=None,aN=None,aCA=None,X=None):
         Psi=Torsion(x0,x1,x2,x3)
         Data.append(Psi)
     return(np.array(Data))
-def GetPhiAtomIndicesAndPosition(C1):
-    a=C1["AtomNames"]
-    aC=np.where(a=="C")[0]
-    aN=np.where(a=="N")[0]
-    aCA=np.where(a=="CA")[0]
-    X=C1["XYZ"]
-    NumResi=C1.GetNumberOfResidues()
-    return(NumResi,a,aC,aN,aCA,X)
+
 
 def GetAllPhi(C1,NumResi=None,a=None,aC=None,aN=None,aCA=None,X=None):
     if a==None:
@@ -94,23 +89,87 @@ def Torsion(x0,x1,x2,x3,Degrees=True):
         phi*=180./np.pi
     return(phi)
 
+def get_angles(pdb_path = "./native.pdb", project_path = "./ProjectInfo.yaml", assignments_path = "./Macro4/MacroAssignments.h5",
+		samplesPerState = 10):
+	# Load conformation and project
+	C1=Conformation.load_from_pdb(pdb_path)
+	P1=Project.load_from(project_path)
+
+	# Extract information from the topology file
+	NumResi,a,aC,aN,aCA,X=GetAtomIndicesAndPosition(C1)
+
+
+	def GetPhi(X):
+		return GetAllPhiDipeptide(C1,NumResi=NumResi,a=a,aC=aC,aN=aN,aCA=aCA,X=X)
+	def GetPsi(X):
+		return GetAllPsiDipeptide(C1,NumResi=NumResi,a=a,aC=aC,aN=aN,aCA=aCA,X=X)
+
+	# Load assignments
+	assign = io.loadh(assignments_path, "arr_0")
+	numStates = assign.max() - assign.min() + 1
+
+	# Lambda function to get max number of conformations per macrostate
+	max_per_state = lambda x: len(np.where(assign == x)[0])
+	# Number of conformations to sample
+	if samplesPerState < 0:
+		num_confs = [max_per_state(i) for i in xrange(numStates)]
+	else:
+		num_confs = [min(samplesPerState, max_per_state(i)) for i in xrange(numStates)]
+	# Total number of conformations
+	num_confs_tot = sum(num_confs)
+
+	print(num_confs)
+
+	# Sample conformations
+	confs = P1.get_random_confs_from_states(assign, range(numStates), num_confs, replacement = False)
+
+	# Initialize arrays
+	phi = np.zeros(num_confs_tot)
+	psi = np.zeros(num_confs_tot)
+	ind = np.array(num_confs, dtype=np.int)
+
+	# Loop through conformations and get phi and psi angles
+	i_tot = 0
+	for c in confs:
+		cxyz = c["XYZList"]
+		for i_conf in xrange(len(cxyz)):
+			phi[i_tot] = GetPhi(c["XYZList"][i_conf])
+			psi[i_tot] = GetPsi(c["XYZList"][i_conf])
+			i_tot+=1
+
+	io.saveh("Dihedrals.h5", Phi = phi, Psi = psi, StateIndex = ind)
+
+	return phi, psi
+
+
 import numpy as np
+from msmbuilder import Conformation,Project,io
+from argparse import ArgumentParser
 import os
-import KyleTools
-from msmbuilder import Conformation,Trajectory,Project,Serializer
 
-C1=Conformation.Conformation.load_from_pdb("./dipeptide.pdb")
-P1=Project.Project.load_from_hdf("./ProjectInfo.h5")
+def main():
+	parser = ArgumentParser(os.path.split(__file__)[1], description = '''
+	A simple script to extract Phi and Psi angles.''')
 
-NumResi,a,aC,aN,aCA,X=GetPhiAtomIndicesAndPosition(C1)
-def GetPhi(X):
-	return GetAllPhiDipeptide(C1,NumResi=NumResi,a=a,aC=aC,aN=aN,aCA=aCA,X=X)
-Phi=P1.EvaluateObservableAcrossProject(GetPhi)
-def GetPsi(X):
-	return GetAllPsiDipeptide(C1,NumResi=NumResi,a=a,aC=aC,aN=aN,aCA=aCA,X=X)
-Psi=P1.EvaluateObservableAcrossProject(GetPsi)
+	parser.add_argument('--pdb', dest = 'pdb_path',
+			help = 'The path to the topology file. Default: native.pdb',
+			default = 'native.pdb', metavar = 'pdb_path')
+	parser.add_argument('-p', '--project', dest='project_path',
+			help = 'MSMBuilder project file. Default: ProjectInfo.yaml',
+			default = 'ProjectInfo.yaml', metavar = 'project')
+	parser.add_argument('-a', dest='assignments_path',
+			help = 'Path to Macrostate assignments. Default: Macro4/MacroAssignments.h5',
+			default = 'Macro4/MacroAssignments.h5', metavar = 'assignments')
+	parser.add_argument('-n', dest = 'samples',
+			help = 'The number of states to sample per macrostate. Use -1 for all. Default: 1000',
+			default=1000, metavar = 'samples', type=int)
 
-print(Phi)
+	args = parser.parse_args()
+	get_angles(pdb_path = args.pdb_path, project_path = args.project_path,
+			assignments_path = args.assignments_path, samplesPerState = args.samples)
 
-Serializer.SaveData("./Phi.h5",Phi)
-Serializer.SaveData("./Psi.h5",Psi)
+if __name__ == '__main__':
+	main()
+
+
+	
