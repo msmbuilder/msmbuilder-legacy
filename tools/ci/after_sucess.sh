fb69faefa422bf7f73fd7dd419b07fb95b56b083
Robert McGibbon <rmcgibbo@gmail.com>
2014-02-03 18:43:38 -0800

    Push to s3

---
+++ tools/ci/after_sucess.sh
@@ -0,0 +1,18 @@
+if [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+    echo "This is a pull request. No deployment will be done."; exit 0
+fi
+
+if [[ `$python -c "import sys; print(sys.version_info[:2])"` != "(2, 7)" ]]; then
+    echo "No deploy on PYTHON_VERSION=${PYTHON_VERSION}"; exit 0
+fi
+
+
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+    echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
+fi
+
+# Create the docs and push them to S3
+sudo apt-get install -qq python-sphinx       # for building documentation
+sudo apt-get install -qq python-boto         # for interacting with S3
+cd docs/sphinx && make html && cd -
+python tools/ci/push-docs-to-s3.py
