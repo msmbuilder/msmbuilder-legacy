bf1a9d68b2d9931fbf170d93773d647a16e0cf87
Robert McGibbon <rmcgibbo@gmail.com>
2014-02-05 12:48:18 -0800

    Change bucket name for S3 docs in preparation for new domain name

--- tools/ci/push-docs-to-s3.py
+++ tools/ci/push-docs-to-s3.py
@@ -6,7 +6,7 @@ from boto.s3.key import Key
 # on travis-ci to push the build documentation to Amazon S3.
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-BUCKET_NAME = 'msmbuilder'
+BUCKET_NAME = 'msmbuilder.org'
 
 bucket_name = AWS_ACCESS_KEY_ID.lower() + '-' + BUCKET_NAME
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
