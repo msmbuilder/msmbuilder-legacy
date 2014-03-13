8a8c6f44eb71731b030c0039f2c2629268a92d7b
Robert McGibbon <rmcgibbo@gmail.com>
2014-03-13 15:42:11 -0700

    remove stuff from setup

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ DOCLINES = __doc__.split("\n")
 
 import os
 import sys
-import shutil
-import tempfile
 import subprocess
 from glob import glob
-from distutils.ccompiler import new_compiler
-from setuptools import setup, Extension
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 try:
     import numpy
@@ -44,7 +44,6 @@ metadata = {
     'url': 'https://simtk.org/home/msmbuilder',
     'download_url': 'https://simtk.org/home/msmbuilder',
     'platforms': ["Linux", "Mac OS X"],
-    'zip_safe': False,
     'description': DOCLINES[0],
     'long_description':"\n".join(DOCLINES[2:]),
     'packages': ['msmbuilder', 'msmbuilder.scripts', 'msmbuilder.project',
@@ -58,54 +57,6 @@ metadata = {
 }
 
 
-def hasfunction(cc, funcname, include=None, extra_postargs=None):
-    # From http://stackoverflow.com/questions/
-    #            7018879/disabling-output-when-compiling-with-distutils
-    tmpdir = tempfile.mkdtemp(prefix='hasfunction-')
-    devnull = oldstderr = None
-    try:
-        try:
-            fname = os.path.join(tmpdir, 'funcname.c')
-            f = open(fname, 'w')
-            if include is not None:
-                f.write('#include %s\n' % include)
-            f.write('int main(void) {\n')
-            f.write('    %s;\n' % funcname)
-            f.write('}\n')
-            f.close()
-            devnull = open(os.devnull, 'w')
-            oldstderr = os.dup(sys.stderr.fileno())
-            os.dup2(devnull.fileno(), sys.stderr.fileno())
-            objects = cc.compile([fname], output_dir=tmpdir,
-                                 extra_postargs=extra_postargs)
-            cc.link_executable(objects, os.path.join(tmpdir, 'a.out'))
-        except Exception as e:
-            return False
-        return True
-    finally:
-        if oldstderr is not None:
-            os.dup2(oldstderr, sys.stderr.fileno())
-        if devnull is not None:
-            devnull.close()
-        shutil.rmtree(tmpdir)
-
-
-def detect_openmp():
-    "Does this compiler support OpenMP parallelization?"
-    compiler = new_compiler()
-    print('Attempting to autodetect OpenMP support...')
-    hasopenmp = hasfunction(compiler, 'omp_get_num_threads()')
-    needs_gomp = hasopenmp
-    if not hasopenmp:
-        compiler.add_library('gomp')
-        hasopenmp = hasfunction(compiler, 'omp_get_num_threads()')
-        needs_gomp = hasopenmp
-    print
-    if hasopenmp:
-        print('Compiler supports OpenMP')
-    else:
-        print('Did not detect OpenMP support; parallel RMSD disabled')
-    return hasopenmp, needs_gomp
 
 # Return the git revision as a string
 # copied from numpy setup.py
@@ -131,6 +82,7 @@ def git_version():
         GIT_REVISION = "Unknown"
 
     return GIT_REVISION
+
 
 def write_version_py(filename='src/python/version.py'):
     cnt = """
@@ -165,27 +117,6 @@ if not release:
         a.close()
 
 
-def main():
-    compiler_args = ['-O3', '-funroll-loops']
-    if new_compiler().compiler_type == 'msvc':
-        compiler_args.append('/arch:SSE2')
+write_version_py()
+setup(**metadata)
 
-    openmp_enabled, needs_gomp = detect_openmp()
-    if openmp_enabled:
-        compiler_args.append('-fopenmp')
-    compiler_libraries = ['gomp'] if needs_gomp else []
-
-    dist = Extension('msmbuilder._distance_wrap', sources=glob('src/ext/scipy_distance/*.c'),
-                     extra_compile_args=compiler_args,
-                     libraries=compiler_libraries,
-                     include_dirs=[numpy.get_include()])
-    contact = Extension('msmbuilder._contact_wrap', sources=glob('src/ext/contact/*.c'),
-                        extra_compile_args=compiler_args,
-                        libraries=compiler_libraries,
-                        include_dirs = [numpy.get_include()])        
-
-    write_version_py()
-    setup(ext_modules=[dist, contact], **metadata)
-
-if __name__ == '__main__':
-    main()
