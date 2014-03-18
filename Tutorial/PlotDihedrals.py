9cea54a4ab3e796e322e86dec4d3ac7846616891
Robert McGibbon <rmcgibbo@gmail.com>
2014-03-17 19:13:57 -0700

    new

--- Tutorial/PlotDihedrals.py
+++ Tutorial/PlotDihedrals.py
@@ -17,60 +17,69 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """Creates Ramachandran plot from raw dipeptide data and macroscopic MSM."""
-from pylab import *
-from numpy import loadtxt
-from matplotlib import *
-from matplotlib.pyplot import *
+
 import sys
-import msmbuilder.io
+import os
+import argparse
+import operator
+import numpy as np
+import matplotlib.pyplot as pp
+
+import mdtraj as md
+from mdtraj import io
+from msmbuilder import  Project
 
 
-if len(sys.argv) != 2:
-    print("Usage: python PlotDihedrals.py Filename")
-    print("Where Filename is the location of the Dihedrals.h5 data file.")
-    sys.exit(0)
-Filename=sys.argv[1]
-
-#Load data
-dihedral_data = msmbuilder.io.loadh(Filename)
-phi = dihedral_data['Phi']
-psi = dihedral_data['Psi']
-ind = dihedral_data['StateIndex']
-NumStates=len(ind)
-
-# Helper function to give the indices for a particular macrostate i
-def w(i, ind):
-    prev = sum(ind[:i])
-    return range(prev, prev+ind[i])
-
-for i in xrange(NumStates):
-    plot(phi[w(i,ind)], psi[w(i,ind)], "x", label="State %d"%i)
-    axis([-180,180,-180,180])
+PSI_INDICES = [6, 8, 14, 16]
+PHI_INDICES = [4, 6, 8, 14]
 
 
-LabelList=[",",".",'o',"<","s","*","h","+","D"]
-ColorList=["b","g","r","c","m","y","k"]
-title("Alanine Dipeptide Macrostates")
-xlabel(r"$\phi$")
-ylabel(r"$\psi$")
-axis([-180,180,-180,180])
-import matplotlib.font_manager
-prop = matplotlib.font_manager.FontProperties(size=8.0)
-legend(loc=0,labelspacing=0.075,prop=prop,scatterpoints=1,markerscale=0.5,numpoints=1)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('macro_model_directory', default='Macro4', help='Path to directory containing a macrostate model. (default=Macro4)')
+    parser.add_argument('--project', default='ProjectInfo.yaml', help='Path to ProjectInfo.yaml file. (default=ProjectInfo.yaml)')
+    args = parser.parse_args()
 
-plot([-180,0],[50,50],'k')
-plot([-180,0],[-100,-100],'k')
-plot([0,180], [100,100],'k')
-plot([0,180], [-50,-50],'k')
-plot([0,0],[-180,180],'k')
-plot([-100,-100],[50,180],'k')
-plot([-100,-100],[-180,-100],'k')
+    project = Project.load_from(args.project)
+    t = reduce(operator.add, (project.load_traj(i) for i in range(project.n_trajs)))
+
+    phi_angles = md.compute_dihedrals(t, [PHI_INDICES]) * 180.0 / np.pi
+    psi_angles = md.compute_dihedrals(t, [PSI_INDICES]) * 180.0 / np.pi
+    state_index = np.hstack(io.loadh(os.path.join(args.macro_model_directory, 'MacroAssignments.h5'))['arr_0'])
+
+    for i in np.unique(state_index):
+        pp.plot(phi_angles[np.where(state_index == i)],
+                psi_angles[np.where(state_index == i)],
+                'x', label='State %d' % i)
 
 
-annotate("PPII",[-30,150],fontsize='x-large')
-annotate(r"$\beta$",[-130,150],fontsize='x-large')
-annotate(r"$\alpha$",[-100,0],fontsize='x-large')
-annotate(r"$\alpha_L$",[100,30],fontsize='x-large')
-annotate(r"$\gamma$",[100,-150],fontsize='x-large')
-show()
+    pp.title("Alanine Dipeptide Macrostates")
+    pp.xlabel(r"$\phi$")
+    pp.ylabel(r"$\psi$")
+    annotate()
 
+    pp.legend(loc=1, labelspacing=0.075, prop={'size': 8.0}, scatterpoints=1,
+              markerscale=0.5, numpoints=1)
+    pp.xlim([-180, 180])
+    pp.ylim([-180, 180])
+    pp.show()
+
+
+def annotate():
+    pp.plot([-180,0],[50,50],'k')
+    pp.plot([-180,0],[-100,-100],'k')
+    pp.plot([0,180], [100,100],'k')
+    pp.plot([0,180], [-50,-50],'k')
+    pp.plot([0,0],[-180,180],'k')
+    pp.plot([-100,-100],[50,180],'k')
+    pp.plot([-100,-100],[-180,-100],'k')
+
+    pp.annotate("PPII",[-30,150],fontsize='x-large')
+    pp.annotate(r"$\beta$",[-130,150],fontsize='x-large')
+    pp.annotate(r"$\alpha$",[-100,0],fontsize='x-large')
+    pp.annotate(r"$\alpha_L$",[100,30],fontsize='x-large')
+    pp.annotate(r"$\gamma$",[100,-150],fontsize='x-large')
+
+
+if __name__ == '__main__':
+    main()
